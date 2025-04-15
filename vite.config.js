3b6306562fb293bbe28d69cf0ed3a3bf846c3bc2
= <=>
2025-04-15 14:46:52 +0530

    updated vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, 'index.html'),       // your main React app
+        background: resolve(__dirname, 'src/storage.js') // background script
+      },
+      output: {
+        entryFileNames: 'background.js', // name files as popup.js, background.js etc
+      }
+    }
+  }
 })
